22063e4afe275b80b9830c90a0e45df965ed97c6
Aguirre Novaes <114250844+aguirrenovaes@users.noreply.github.com>
2023-05-03 14:34:55 -0300

    Atualizar Conexao Para Azure

--- php/conexao.php
+++ php/conexao.php
@@ -1,7 +1,7 @@
 <?php 
-$server = "localhost";
-$user = "root";
-$password = "";
+$server = "db-server-aguirre.mysql.database.azure.com";
+$user = "root1";
+$password = "Senai@115";
 $dbname = "estacionamento";
 
 $con = mysqli_connect($server, $user, $password, $dbname);
